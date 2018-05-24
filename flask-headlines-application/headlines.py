8bdfa4b305518521e47fdfd6856c220e7f2c5274
rahiakela <rahi.akela@yahoo.com>
2018-05-24 14:12:54 +0530

    adding form for rss feed search'

--- flask-headlines-application/headlines.py
+++ flask-headlines-application/headlines.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import feedparser
 
 app = Flask(__name__)
@@ -12,8 +12,14 @@ RSS_FEEDS = {
 
 
 @app.route('/')
-@app.route('/<publication>')
-def get_news(publication='bbc'):
+def get_news():
+    query = request.args.get('publication')
+
+    if not query or query.lower() not in RSS_FEEDS:
+        publication = 'bbc'
+    else:
+        publication = query.lower()
+
     feed = feedparser.parse(RSS_FEEDS[publication])
 
     return render_template('home.html', articles=feed['entries'])
