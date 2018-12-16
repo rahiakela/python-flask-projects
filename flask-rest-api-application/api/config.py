ef222a713a55be5e0fd88265b8217a81a03f778e
rahiakela <rahi.akela@yahoo.com>
2018-12-16 19:27:36 +0530

    removing credetials

--- flask-rest-api-application/api/config.py
+++ flask-rest-api-application/api/config.py
@@ -7,5 +7,5 @@ PORT = 5000
 HOST = "127.0.0.1"
 SQLALCHEMY_ECHO = False
 SQLALCHEMY_TRACK_MODIFICATIONS = True
-SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{PORT}/{DB_NAME}".format(DB_USER='rakela', DB_PASSWORD='mysonisryaan', DB_HOST='35.224.193.245', PORT='3306', DB_NAME='messages')
+SQLALCHEMY_DATABASE_URI = "mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{PORT}/{DB_NAME}".format(DB_USER='', DB_PASSWORD='', DB_HOST='35.224.193.245', PORT='3306', DB_NAME='messages')
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
